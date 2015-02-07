52fc074e0a2c0580e6e52c5f2036d04c3c4036f9
Kento Kobayashi <koba@koba.local>
2015-02-07 16:58:25 +0900

    fix registrable_check

--- app/src/main/java/eval/wit/ai/calcmahjong/activities/PlayerRegistrationActivity.java
+++ app/src/main/java/eval/wit/ai/calcmahjong/activities/PlayerRegistrationActivity.java
@@ -50,45 +50,36 @@ public class PlayerRegistrationActivity extends ActionBarActivity {
         public void onClick(View v) {
             databaseAdapter.open();
 
-            if (registrationStatusCheckByToast()) {
-                databaseAdapter.savePlayer(nameTxt.getText().toString(), messageTxt.getText().toString());
-                finish();
+            // 名前有無判定
+            if (nameTxt.getText().toString().equals("")) {
+                UiUtil.showToast(PlayerRegistrationActivity.this,
+                        getResources().getString(R.string.name_nothing_message));
+                databaseAdapter.close();
+                return;
             }
 
+            // 名前の重複チェック
+            if (databaseAdapter.isDuplicationPlayer(nameTxt.getText().toString())) {
+                UiUtil.showToast(PlayerRegistrationActivity.this,
+                        getResources().getString(R.string.name_duplication_message));
+                databaseAdapter.close();
+                return;
+            }
+
+            // 入力文字数制限
+            if (nameTxt.getText().toString().length() > 4) {
+                UiUtil.showToast(PlayerRegistrationActivity.this,
+                        getResources().getString(R.string.number_of_name_over_message));
+                databaseAdapter.close();
+                return;
+            }
+
+            databaseAdapter.savePlayer(nameTxt.getText().toString(), messageTxt.getText().toString());
             databaseAdapter.close();
+
+            finish();
         }
     };
-
-    /**
-     * 登録可能か確認し、不可能ならその旨をトーストで表示します。
-     * @return 登録可否
-     */
-    private boolean registrationStatusCheckByToast() {
-        boolean registerable = true;
-
-        // 名前の重複チェック
-        if (databaseAdapter.isDuplicationPlayer(nameTxt.getText().toString())) {
-            UiUtil.showToast(PlayerRegistrationActivity.this,
-                    getResources().getString(R.string.name_duplication_message));
-            registerable = false;
-        }
-
-        // 名前有無判定
-        if (nameTxt.getText().toString().equals("")) {
-            UiUtil.showToast(PlayerRegistrationActivity.this,
-                    getResources().getString(R.string.name_nothing_message));
-            registerable = false;
-        }
-
-        // 入力文字数制限
-        if (nameTxt.getText().toString().length() > 5) {
-            UiUtil.showToast(PlayerRegistrationActivity.this,
-                    getResources().getString(R.string.number_of_name_over_message));
-            registerable = false;
-        }
-
-        return registerable;
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
